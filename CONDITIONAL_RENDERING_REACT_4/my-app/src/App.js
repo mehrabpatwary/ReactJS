2739791a640d4ebc52fe0ff545dc7e85d2a65df2
Mehrab Patwary <39481182+mehrabpatwary@users.noreply.github.com>
2021-08-14 13:46:39 +0600

    Add files via upload

---
+++ CONDITIONAL_RENDERING_REACT_4/my-app/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import CONDITIONAL_RENDERING from './CONDITIONAL_RENDERING/index'
+
+export default function App() {
+  return (
+    <div>
+      <CONDITIONAL_RENDERING />
+    </div>
+  )
+}
