fbde79d1a6f64aa5154a3ba19dcdcdfd109c3b2f
Mehrab Patwary <39481182+mehrabpatwary@users.noreply.github.com>
2021-08-14 13:45:35 +0600

    Add files via upload

---
+++ PROPS_REACT_2/my-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Card1 from './components/Card1'
+import Card2 from './components/Card2'
+
+function App() {
+  return (<div>
+    <Card1 name="Mehrab" desc="BSc. in CSE"/>
+    <Card2 name="Card2"/>
+  </div>);
+}
+export default App;
---
+++ PROPS_REACT_2/my-app/src/components/Card1.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Card1(props) {
+    return (
+        <div>{props.name} {props.desc}</div>
+    );
+}
+export default Card1;
